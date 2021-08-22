1dea9c516f21eea939418c9a08d3f0428d82a6b2
danoux10 <danybarbe10@gmail.com>
2021-08-22 16:32:41 +0200

    correction lien icon

--- page/Ratafia.php
+++ page/Ratafia.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../style.css" />
     <title>Didier Barbe Entreprise | Ratafia</title>
-    <link rel="shortcut icon" href="media/logo/dbe.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../media/logo/dbe.ico" type="image/x-icon">
   </head>
   <body>
     <header class="header-content">
